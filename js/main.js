30eef3a421974b5125b22ceeb023088df82e62d8
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-03-30 11:04:23 -0300

    pequenos ajustes

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ const btnReservar = document.querySelector('#btn-reserva')
 let condition = false
 btnReservar.onclick = (e) =>{
         e.preventDefault();
-        console.log(e.target);
         modal.setAttribute('style', 'display:block')
         blur.setAttribute('style', 'filter:blur(5px)')
         setTimeout(() =>{
